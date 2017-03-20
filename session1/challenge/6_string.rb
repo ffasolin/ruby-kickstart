6c6ea6aa253aa45b4f0bf909b5a1c395cbc9290a
Filipe Fasolin <ffasolin@gmail.com>
2017-03-20 19:37:01 +0000

    Session1 - challenge 6 finally done

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -7,5 +7,40 @@
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
+# def odds_and_evens(string, return_odds)
+# new_string = ""
+# string.length.times do |i|
+# 	string[i] = if i.odd?
+# 					new_string << string[i]
+# 				elsif i.even?
+# 					new_string << string[i]
+# 				end
+# end
+# new_string
+# end
+
 def odds_and_evens(string, return_odds)
+  new_string = ""
+  string.length.times do |i|
+    if return_odds && i.odd?
+    new_string << string[i]
+    elsif !return_odds && i.even?
+    new_string << string[i]
+  end
 end
+  new_string
+end
+
+puts odds_and_evens("abcdefg",true)    # => "bdf"
+#puts odds_and_evens("abcdefg",false)   # => "aceg"
+
+
+# new_string = ""
+#   string.size.times do |i|
+#   	if return_odds && i.odd?
+#   		new_string << i.next
+#   	elsif return_odds == false && i.even?
+#   		new_string << i
+#   	end
+#   	new_string
+#   end
