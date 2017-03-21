56be10e0b6f02145989bd9a51ce275f39c369dda
Filipe Fasolin <ffasolin@gmail.com>
2017-03-21 18:40:13 +0000

    Session2 - fifth challenge done

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -13,3 +13,16 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+
+def mod_three (array)
+	new_array = Array.new
+	array.each do |i|
+		if i % 3 != 0
+		new_array << (i % 3)
+	end
+	end
+	new_array
+end
+
+myarray = [1, 2, 3, 4, 5, 6, 7]
+p mod_three(myarray)
