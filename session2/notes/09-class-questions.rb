43d69f81cc868428ce65c2decd41174628043131
Filipe Fasolin <ffasolin@gmail.com>
2017-03-21 12:02:29 +0000

    Session2 - class questions answered

--- session2/notes/09-class-questions.rb
+++ session2/notes/09-class-questions.rb
@@ -1,13 +1,19 @@
 # Between Yankees and Baseball team
 # which is a class and which is an instance?
 
+# Baseball is a class; Yankee is an instance
+
 # Between Person and Enoch
 # which is a class and which is an instance?
 
+# Person is a class, Enoch is an instance
+
 # Can you think of another class and several instances?
 
-
+# class Operations, instances sum, multiply, divide, subtract
 
 # Are classes objects?
 # If no, where do you think their methods are stored?
 # If yes, what class are they instances of?
+
+# Yes. Instances of the Class class.
