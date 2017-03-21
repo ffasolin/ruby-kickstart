97d498a580b05e4deaaafaa9c211e9b4eb478e48
Filipe Fasolin <ffasolin@gmail.com>
2017-03-21 11:24:29 +0000

    Session1 - seventh challenge done

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,15 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	new_string = ""
+	(string.length - 1).times do |i|
+		if (string[i] == "r" || string[i] == "R")
+		new_string << string[i + 1]
+		end
+	end
+new_string
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+# || string[i] == "R")
