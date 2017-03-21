927ed1839df403a08b50852414f6d897fee1fe30
Filipe Fasolin <ffasolin@gmail.com>
2017-03-21 17:56:25 +0000

    Session2 - third challende done

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,7 +5,30 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+
+
+
+
+
+
 class String
+	# def initialize (string)
+	# 	@string = string
+	# end
+	# attr_accessor :string
   def every_other_char
+  	new_string = ""
+  	new_array = split("")
+  	new_array.length.times do |i|
+  		if i.even?
+  			new_string << new_array[i]
+   		end
+  	end
+  	new_string
   end
 end
+
+example = String.new ("abcdefg")
+puts example.every_other_char
+example1 = String.new ("")
+puts example1.every_other_char
