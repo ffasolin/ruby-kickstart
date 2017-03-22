eb064712431f60faaeb46106eb66e4e3c8b39320
Filipe Fasolin <ffasolin@gmail.com>
2017-03-22 18:43:03 +0000

    Session2 - eighth challenge done

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,15 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three? (array)
+	array.each_cons 3 do |elem1, elem2, elem3|
+		return true if (elem1 == elem2) && (elem2 == elem3)
+		end
+		false
+end
+
+p got_three? [1, 2, 2, 2, 3]  # => true
+p got_three? ['a', 'a', 'b']  # => false
+p got_three? ['a', 'a', 'a']  # => true
+p got_three? [1, 2, 1, 1]     # => false
