e3f942c41b0ca485f17d13cb0c72856eacbff3c0
Filipe Fasolin <ffasolin@gmail.com>
2017-03-23 19:28:58 +0000

    Session3 challenge 2 done

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,43 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase (i)
+
+# 	for n in 1..i
+# 	h = Hash.new
+# 	if n.odd?
+# 		par = []
+# 		for n in 1..i
+# 			if n.even?
+# 				par << n
+# 			end
+# 		end
+# 		h[n] = par
+# 	end
+# 	h
+# end
+# end
+h = Hash.new
+	for n in 1..i
+		if n.odd?
+			eveni = []
+			for d in 1..n
+				if d.even?
+					eveni << d
+				end
+			end
+			h[n] = [eveni].flatten
+		end
+	end
+	h
+end
+# 		eveni = []
+# 		for n in 1..(oddi.max)
+# 			if n.even?
+# 				eveni << n
+# 			end
+# 		end
+# 	h = Hash[ *[ Array(oddi) , Array(eveni) ] ]
+# end
+
+p staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
