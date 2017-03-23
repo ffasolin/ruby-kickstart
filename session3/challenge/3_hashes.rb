cfb4111736f704e090584b2ae09065efe660c69a
Filipe Fasolin <ffasolin@gmail.com>
2017-03-23 19:54:34 +0000

    Session3 challenge 3 done

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,9 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str)
+	h = Hash.new(0)
+	str.downcase.split.each { |word| h[word] += 1 }
+	h
+end
