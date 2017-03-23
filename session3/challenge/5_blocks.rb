f16705bf504ea751e8c71e63dddcb0803bbada18
Filipe Fasolin <ffasolin@gmail.com>
2017-03-23 22:12:58 +0000

    Session3 challenge 5 done

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,20 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+def spiral_access (arr, &block)
+	arr.flatten.sort.each(&block)
+end
+
+two_d = [
+  [ 1,  2,  3,  4, 5],
+  [16, 17, 18, 19, 6],
+  [15, 24, 25, 20, 7],
+  [14, 23, 22, 21, 8],
+  [13, 12, 11, 10, 9],
+]
+order = []
+spiral_access two_d do |i|
+  order << i
+end
+p order
