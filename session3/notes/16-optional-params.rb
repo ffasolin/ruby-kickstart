5d1e342ecccb228285286266ae4a7296a39654e6
Filipe Fasolin <ffasolin@gmail.com>
2017-03-23 10:27:30 +0000

    Session3 - note 16 - what it really returns

--- session3/notes/16-optional-params.rb
+++ session3/notes/16-optional-params.rb
@@ -24,4 +24,4 @@ what_are_filled_in :A , :B , :C , :D       # => "A B C D 1"
 what_are_filled_in :A , :B , :C , :D , :E  # => "A B C D E"
 
 # what will this return?
-what_are_filled_in :A ,                :E  # => "A 4 3 2 E"
+what_are_filled_in :A ,                :E  # => "A E 3 2 1"
