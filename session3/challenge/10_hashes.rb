77126c581bc245d0f66abd1f7732a1c5e9b7cf42
Filipe Fasolin <ffasolin@gmail.com>
2017-03-25 19:12:46 +0000

    Session3 challenge 10 done

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -29,5 +29,92 @@
 # create it from scratch :)
 
 
-def pathify
+def pathify(hashx)
+	arr = []
+	return hashx.map { |item| "/" + item } if hashx.is_a? Array
+		
+	hashx.each do |a, b|
+	arr2 = pathify(b)
+	arr2.each do |c|
+		arr << (a + c)
+	end
+	end
+	arr.map { |slash| "/" + slash }
 end
+
+
+
+# arr = []
+# 	hashx.each do |key, value|
+# 		arr << key
+# 			if value.is_a? Array
+# 				arr << value
+# 			else
+# 				tree = pathify(value)
+# 				tree.each do |key1, value1|
+# 					arr << (key1 + value1)
+# 				end
+# 			end
+# 		end
+# 		arr
+# 		end
+
+
+
+
+			# value.each do |key1, value1|
+			# 	if value1.is_a? Array
+			# 		arr << key1
+			# 		arr << value1
+			# 		# if value1.count > 1
+			# 		# 	arr.flatten.pop[-1]
+			# 		# end
+			# 	elsif  value1.is_a? Hash
+			# 		arr << pathify(value1)
+			# 	end
+			# end
+			#end
+
+
+
+
+
+	# 	value.each do |key1, value1|
+	# 		arr << key1
+	# 		if value1.count == 1
+	# 		    arr << value1
+	# 		elsif value1.count > 1
+	# 			value1.each do |a, b|
+	# 				arr << a
+	# 				arr.delete(a) << b
+	# 			end
+	# 		end
+	# 	end
+	# end
+	# final_arr = []
+	# final_arr << arr.join("/")
+	# p final_arr
+
+	# final_arr = []
+	# hashx = hashx.flatten
+	# final_arr << hashx.keys
+	# final_arr << hashx.values
+	# final_arr.join
+
+
+
+
+
+p pathify 'usr' => {'bin' => ['ruby'] }                                                      # => ['/usr/bin/ruby']
+p pathify 'usr' => {'bin' => ['ruby', 'perl'] }                                                # => ['/usr/bin/ruby', '/usr/bin/perl']
+p pathify 'usr' => {'bin' => ['ruby'], 'include' => ['zlib.h'] }                               # => ['/usr/bin/ruby', '/usr/include/zlib.h']
+p pathify 'usr' => {'bin' => ['ruby']}, 'opt' => {'local' => {'bin' => ['sqlite3', 'rsync']} } # => ['/usr/bin/ruby', 'opt/local/bin/sqlite3', 'opt/local/bin/rsync'] 
+
+
+
+
+
+
+
+
+
